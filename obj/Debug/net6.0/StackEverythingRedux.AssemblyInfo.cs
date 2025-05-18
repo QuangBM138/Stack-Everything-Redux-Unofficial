a72b4824668c4547d3c05cabf09fede03955d874
QuangBM138 <155440479+QuangBM138@users.noreply.github.com>
2025-05-18 07:35:20 +0700

    drop commit "update"

--- obj/Debug/net6.0/StackEverythingRedux.AssemblyInfo.cs
+++ obj/Debug/net6.0/StackEverythingRedux.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StackEverythingRedux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af737afac967f8d89ab141250b4000d2da08964d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+603a7663e75f4711e69fff4cda636336293f0b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("StackEverythingRedux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StackEverythingRedux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
